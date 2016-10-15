c63c322ee6094745ff87d90e55284a6f2041b81f
JMauris <justine.mauris@students.hevs.ch>
2016-10-15 15:07:04 +0200

    first

---
+++ views/login/welcome.php
@@ -0,0 +1,23 @@
+<?php
+include_once ROOT_DIR.'global/header.php';
+
+//Collect data from controller and session
+$msg = $this->vars['msg'];
+$user = $_SESSION['personne'];
+?>
+<br><br>
+	<table align="center">
+		<tr>
+			<td>
+				<?php echo $msg;?>
+				<h1>Welcome <?php echo ' '.$user->getFirstname().' '.$user->getLastname();?> </h1></h1>
+				<a href="<?php echo URL_DIR.'login/logout';?>">Logout</a>
+			</td>
+		</tr>
+	</table>
+</form>
+<br/><br/><br/><br/>
+<?php
+unset($_SESSION['msg']);
+include_once ROOT_DIR.'global/footer.php';
+?>
